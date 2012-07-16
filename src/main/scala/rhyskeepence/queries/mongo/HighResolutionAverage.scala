8b17afdfbcc7e780ca1e523eb049e3ec5676cd4b
Rhys Keepence <rhyskeepence@gmail.com>
2012-07-16 12:41:28 +0100

    fixed high res aggregator so that it really only does super high res for same day charts

--- src/main/scala/rhyskeepence/queries/mongo/HighResolutionAverage.scala
+++ src/main/scala/rhyskeepence/queries/mongo/HighResolutionAverage.scala
@@ -7,8 +7,8 @@ class HighResolutionAverage extends AverageAggregator {
   override def aggregate(environment: String, metricName: String, interval: Interval) = {
 
     val sizeOfEachBucket =
-      if (interval.toDurationMillis < 86400000)
-        10000  // 10 second buckets for less than 24 hrs (same day)
+      if (interval.toDurationMillis < 86399999)
+        30000  // 30 second buckets for less than 24 hrs (same day)
       else
         300000 // 5 minute buckets for 24 hr charts
 
